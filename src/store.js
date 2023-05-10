3e2a353a7d63379b0b00f334934dd31c121a54cf
kajitomg <elementrixmg@bk.ru>
2023-05-10 23:50:01 +0300

    Доработаны выделения записей, индексация, и сделан счетчик нажатий по ним

--- src/store.js
+++ src/store.js
@@ -1,79 +1,92 @@
+import { createCounter } from "./utils";
+
 /**
  * Хранилище состояния приложения
  */
+const indexGenerator = createCounter(8) // Функция генерации уникального индекса для элемента списка
+
 class Store {
-  constructor(initState = {}) {
-    this.state = initState;
-    this.listeners = []; // Слушатели изменений состояния
-  }
+	constructor(initState = {}) {
+		this.state = initState;
+		this.listeners = []; // Слушатели изменений состояния
+	}
 
-  /**
-   * Подписка слушателя на изменения состояния
-   * @param listener {Function}
-   * @returns {Function} Функция отписки
-   */
-  subscribe(listener) {
-    this.listeners.push(listener);
-    // Возвращается функция для удаления добавленного слушателя
-    return () => {
-      this.listeners = this.listeners.filter(item => item !== listener);
-    }
-  }
+	/**
+	 * Подписка слушателя на изменения состояния
+	 * @param listener {Function}
+	 * @returns {Function} Функция отписки
+	 */
+	subscribe(listener) {
+		this.listeners.push(listener);
+		// Возвращается функция для удаления добавленного слушателя
+		return () => {
+			this.listeners = this.listeners.filter(item => item !== listener);
+		}
+	}
 
-  /**
-   * Выбор состояния
-   * @returns {Object}
-   */
-  getState() {
-    return this.state;
-  }
+	/**
+	 * Выбор состояния
+	 * @returns {Object}
+	 */
+	getState() {
+		return this.state;
+	}
 
-  /**
-   * Установка состояния
-   * @param newState {Object}
-   */
-  setState(newState) {
-    this.state = newState;
-    // Вызываем всех слушателей
-    for (const listener of this.listeners) listener();
-  }
+	/**
+	 * Установка состояния
+	 * @param newState {Object}
+	 */
+	setState(newState) {
+		this.state = newState;
+		// Вызываем всех слушателей
+		for (const listener of this.listeners) listener();
+	}
 
-  /**
-   * Добавление новой записи
-   */
-  addItem() {
-    this.setState({
-      ...this.state,
-      list: [...this.state.list, {code: this.state.list.length + 1, title: 'Новая запись'}]
-    })
-  };
+	/**
+	 * Добавление новой записи
+	 */
+	addItem() {
+		this.setState({
+			...this.state,
+			list: [...this.state.list, { code: indexGenerator(), title: 'Новая запись' }]
+		})
+	};
 
-  /**
-   * Удаление записи по коду
-   * @param code
-   */
-  deleteItem(code) {
-    this.setState({
-      ...this.state,
-      list: this.state.list.filter(item => item.code !== code)
-    })
-  };
+	/**
+	 * Удаление записи по коду
+	 * @param code
+	 */
+	deleteItem(code) {
+		this.setState({
+			...this.state,
+			list: this.state.list.filter(item => item.code !== code)
+		})
+	};
 
-  /**
-   * Выделение записи по коду
-   * @param code
-   */
-  selectItem(code) {
-    this.setState({
-      ...this.state,
-      list: this.state.list.map(item => {
-        if (item.code === code) {
-          item.selected = !item.selected;
-        }
-        return item;
-      })
-    })
-  }
+	/**
+	 * Выделение записи по коду
+	 * @param code
+	 */
+	selectItem(code) {
+		this.setState({
+			...this.state,
+			list: this.state.list.map(item => {
+				if (item.code !== code) { // Проверка для исключения случая клика по уже выбранному элементу
+					item.selected = false
+				}
+				if (item.code === code) {
+					if (!item.selected) {
+						if (!item.selections) {
+							item.selections = 0 // Инициируем поле, так как в исходном массиве его нет
+						}
+						item.selections++
+					}
+					item.selected = !item.selected;
+				}
+				return item;
+			})
+		})
+	}
 }
 
 export default Store;
--- src/utils.js
+++ src/utils.js
@@ -8,21 +8,57 @@ const propNames = new Set(['id', 'className', 'textContent', 'onclick']);
  * @returns {HTMLElement}
  */
 export function createElement(name, props = {}, ...children) {
-  const element = document.createElement(name);
+	const element = document.createElement(name);
 
-  // Назначение свойств и атрибутов
-  for (const name of Object.keys(props)) {
-    if (propNames.has(name)) {
-      element[name] = props[name];
-    } else {
-      element.setAttribute(name, props[name]);
-    }
-  }
+	// Назначение свойств и атрибутов
+	for (const name of Object.keys(props)) {
+		if (propNames.has(name)) {
+			element[name] = props[name];
+		} else {
+			element.setAttribute(name, props[name]);
+		}
+	}
 
-  // Вставка вложенных элементов
-  for (const child of children) {
-    element.append(child);
-  }
+	// Вставка вложенных элементов
+	for (const child of children) {
+		element.append(child);
+	}
 
-  return element;
+	return element;
 }
+
+/**
+ * Создание счетчика
+ * @param firstIndex {Number} Число с которого начинается счетчик
+ * @returns {Function}
+ */
+export function createCounter(firstIndex) {
+
+	let index = firstIndex; // Замыкаем начальноого значения счетчика при вызове функции counter
+
+	return function () {
+
+		return index++
+	}
+}
+
+/**
+ * Создание форматора
+ * @param one {String} Пример склонения слова с 'один'
+ * @param two {String} Пример склонения слова с 'два'
+ * @param few {String} Пример склонения слова с 'несколько'
+ * @returns {Function}
+ */
+export function createStringFormatter(one, two, few) {
+	const cases = [2, 0, 1, 1, 1, 2];
+	const closedTitles = [one, two, few];
+
+	return (number) => {
+		return closedTitles[
+			number % 100 > 4 && number % 100 < 20
+				? 2
+				: cases[number % 10 < 5 ? number % 10 : 5]
+		];
+	};
+}
+
