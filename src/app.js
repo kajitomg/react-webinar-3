cfc32284ed84baa7f77145eba0276867256c33d8
kajitomg <elementrixmg@bk.ru>
2023-05-11 23:36:09 +0300

    Фикс бага с табуляцией - переход на webshtorm

--- src/app.js
+++ src/app.js
@@ -9,45 +9,51 @@ import './styles.css';
  */
 function App({ store }) {
 
-	const list = store.getState().list;
-	const quantityFormatter = createStringFormatter('раз', 'раза', 'раз')
+  const list = store.getState().list;
+  const quantityFormatter = createStringFormatter('раз', 'раза', 'раз')
 
-	return (
-		<div className='App'>
-			<div className='App-head'>
-				<h1>Приложение на чистом JS</h1>
-			</div>
-			<div className='App-controls'>
-				<button onClick={() => store.addItem()}>Добавить</button>
-			</div>
-			<div className='App-center'>
-				<div className='List'>{
-					list.map(item =>
-						<div key={item.code} className='List-item'>
-							<div className={'Item' + (item.selected ? ' Item_selected' : '')}
-								onClick={() => store.selectItem(item.code)}>
-								<div className='Item-left'>
-									<div className='Item-code'>{item.code}</div>
-									<div className='Item-title'>{item.title}</div>
-									{item.selections &&
-										<>
-											<div className='Item-splitter'>|</div>
-											<div className='Item-counter'>Выделяли {item.selections} {quantityFormatter(item.selections)}</div>
-										</>
-									}
-								</div>
-								<div className='Item-actions'>
-									<button onClick={() => store.deleteItem(item.code)}>
-										Удалить
-									</button>
-								</div>
-							</div>
-						</div>
-					)}
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div className='App'>
+      <div className='App-head'>
+        <h1>Приложение на чистом JS</h1>
+      </div>
+      <div className='App-controls'>
+        <button
+          onClick={() => store.addItem()}
+        >Добавить</button>
+      </div>
+      <div className='App-center'>
+        <div className='List'>{
+          list.map(item =>
+            <div key={item.code} className='List-item'>
+              <div
+                className={'Item' + (item.selected ? ' Item_selected' : '')}
+                onClick={() => store.selectItem(item.code)}
+              >
+                <div className='Item-left'>
+                  <div className='Item-code'>{item.code}</div>
+                  <div className='Item-title'>{item.title}</div>
+                  {item.selections &&
+                    <>
+                      <div className='Item-splitter'>|</div>
+                      <div className='Item-counter'>Выделяли {item.selections} {quantityFormatter(item.selections)}</div>
+                    </>
+                  }
+                </div>
+                <div className='Item-actions'>
+                  <button
+                    onClick={() => store.deleteItem(item.code)}
+                  >
+                    Удалить
+                  </button>
+                </div>
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- src/store.js
+++ src/store.js
@@ -1,4 +1,4 @@
-import { createCounter } from "./utils";
+import {createCounter} from "./utils";
 
 /**
  * Хранилище состояния приложения
@@ -6,98 +6,98 @@ import { createCounter } from "./utils";
 const indexGenerator = createCounter(8) // Функция генерации уникального индекса для элемента списка
 
 class Store {
-	constructor(initState = {}) {
-		this.state = initState;
-		this.listeners = []; // Слушатели изменений состояния
-	}
+  constructor(initState = {}) {
+    this.state = initState;
+    this.listeners = []; // Слушатели изменений состояния
+  }
 
-	/**
-	 * Подписка слушателя на изменения состояния
-	 * @param listener {Function}
-	 * @returns {Function} Функция отписки
-	 */
-	subscribe(listener) {
-		this.listeners.push(listener);
-		// Возвращается функция для удаления добавленного слушателя
-		return () => {
-			this.listeners = this.listeners.filter(item => item !== listener);
-		}
-	}
+  /**
+   * Подписка слушателя на изменения состояния
+   * @param listener {Function}
+   * @returns {Function} Функция отписки
+   */
+  subscribe(listener) {
+    this.listeners.push(listener);
+    // Возвращается функция для удаления добавленного слушателя
+    return () => {
+      this.listeners = this.listeners.filter(item => item !== listener);
+    }
+  }
 
-	/**
-	 * Выбор состояния
-	 * @returns {Object}
-	 */
-	getState() {
-		return this.state;
-	}
+  /**
+   * Выбор состояния
+   * @returns {Object}
+   */
+  getState() {
+    return this.state;
+  }
 
-	/**
-	 * Установка состояния
-	 * @param newState {Object}
-	 */
-	setState(newState) {
-		this.state = newState;
-		// Вызываем всех слушателей
-		for (const listener of this.listeners) listener();
-	}
+  /**
+   * Установка состояния
+   * @param newState {Object}
+   */
+  setState(newState) {
+    this.state = newState;
+    // Вызываем всех слушателей
+    for (const listener of this.listeners) listener();
+  }
 
-	/**
-	 * Добавление новой записи
-	 */
-	addItem() {
-		this.setState({
-			...this.state,
-			list: [...this.state.list, { code: indexGenerator(), title: 'Новая запись' }]
-		})
-	};
+  /**
+   * Добавление новой записи
+   */
+  addItem() {
+    this.setState({
+      ...this.state,
+      list: [...this.state.list, {code: indexGenerator(), title: 'Новая запись'}]
+    })
+  };
 
-	/**
-	 * Удаление записи по коду
-	 * @param code {Number}
-	 */
-	deleteItem(code) {
-		this.setState({
-			...this.state,
-			list: this.state.list.filter(item => item.code !== code)
-		})
-	};
+  /**
+   * Удаление записи по коду
+   * @param code {Number}
+   */
+  deleteItem(code) {
+    this.setState({
+      ...this.state,
+      list: this.state.list.filter(item => item.code !== code)
+    })
+  };
 
-	/**
-	 * Выделение записи по коду
-	 * @param code {Number}
-	 */
-	selectItem(code) {
-		this.setState({
-			...this.state,
-			list: this.state.list.map(item => {
-				if (item.code !== code) { // Проверка для исключения случая клика по уже выбранному элементу
-					item.selected = false
-				}
-				if (item.code === code) {
-					item.selections = this.#getIncreaseCounter(item)
-					item.selected = !item.selected;
-				}
-				return item;
-			})
-		})
-	}
+  /**
+   * Выделение записи по коду
+   * @param code {Number}
+   */
+  selectItem(code) {
+    this.setState({
+      ...this.state,
+      list: this.state.list.map(item => {
+        if (item.code !== code) { // Проверка для исключения случая клика по уже выбранному элементу
+          item.selected = false
+        }
+        if (item.code === code) {
+          item.selections = this.#getIncreaseCounter(item)
+          item.selected = !item.selected;
+        }
+        return item;
+      })
+    })
+  }
 
-	/**
-	 * Увеличение значения счетчика
-	 * @param item {Object}
-	 * @returns {Number} Увеличенное значение счетчика или его инициализация
-	 */
-	#getIncreaseCounter(item) {
-		if (!item.selected) { // Проверка, так как повышение счетчика только при клике по невыбранному элементу
-			if (item.selections === undefined) {
-				return 1 // Инициируем поле, так как в исходном массиве его нет
-			}
-			return item.selections + 1
-		}
-		return item.selections;
+  /**
+   * Увеличение значения счетчика
+   * @param item {Object}
+   * @returns {Number} Увеличенное значение счетчика или его инициализация
+   */
+  #getIncreaseCounter(item) {
+    if (!item.selected) { // Проверка, так как повышение счетчика только при клике по невыбранному элементу
+      if (item.selections === undefined) {
+        return 1 // Инициируем поле, так как в исходном массиве его нет
+      }
+      return item.selections + 1
+    }
+    return item.selections;
 
-	}
+  }
 
 }
 
