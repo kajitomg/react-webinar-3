2ae8ee2009f25ccc526e6b8df69a5b65889fb6d8
kajitomg <elementrixmg@bk.ru>
2023-06-10 15:04:33 +0300

    ÑКРедирект после авторизации

--- src/components/comments-auth/index.js
+++ src/components/comments-auth/index.js
@@ -1,11 +1,12 @@
 import {memo} from "react";
 import {cn as bem} from '@bem-react/classname';
 import PropTypes from "prop-types";
-import {Link} from "react-router-dom";
+import {Link, useLocation} from "react-router-dom";
 import './style.css';
 
 function CommentsAuth(props){
   const cn = bem('CommentsAuth');
+  const location = useLocation()
 
   const callbacks = {
     onClose: () => {
@@ -15,7 +16,7 @@ function CommentsAuth(props){
 
   return (
     <div className={cn({nested:props.nested})}>
-      <span className={cn('text')}><Link to={'/login'}>Войдите</Link>, {props.text}</span>
+      <span className={cn('text')}><Link to={'/login'} state={{back:location.pathname}}>Войдите</Link>, {props.text}</span>
       {props.button && <button className={cn('button')} role={'button'} onClick={callbacks.onClose}>{props.button}</button>}
     </div>
   );
